ad597f0088fbd1d61f0b77c0e22ab0be2510484f
kristof.renders <kristof.renders@dynatrace.com>
2021-03-03 18:57:17 +0100

    fix output

--- microk8s/terraform/outputs.tf
+++ microk8s/terraform/outputs.tf
@@ -1,7 +1,7 @@
 output "acebox_dashboard" {
-  value = "http://dashboard.$(google_compute_instance.acebox.network_interface[0].access_config[0].nat_ip).nip.io"
+  value = "http://dashboard.${google_compute_instance.acebox.network_interface[0].access_config[0].nat_ip}.nip.io"
 }
 
 output "acebox_ip" {
-  value = "connect using ssh -i [location of key file] ${var.acebox_user}@$(google_compute_instance.acebox.network_interface[0].access_config[0].nat_ip)"
+  value = "connect using ssh -i [location of key file] ${var.acebox_user}@${google_compute_instance.acebox.network_interface[0].access_config[0].nat_ip}"
 }
