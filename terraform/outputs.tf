011293b7adf37046b2e12368405bce98e24d9bae
Tobi Gremmer <tobias.gremmer@dynatrace.com>
2021-05-25 16:45:00 +0200

    show config post installation

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,7 @@ output "acebox_dashboard" {
 output "acebox_ip" {
   value = "connect using ssh -i [location of key file] ${var.acebox_user}@${google_compute_instance.acebox.network_interface[0].access_config[0].nat_ip}"
 }
+
+output "comment" {
+  value = "More information about dashboard credentials is printed out as part of the last provisioning step. Please scroll up."
+}
