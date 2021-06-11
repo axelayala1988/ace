445ff01dff69f32caec23de0808fb637dbb82ab8
Tobi Gremmer <tobias.gremmer@dynatrace.com>
2021-06-11 15:18:25 +0200

    get nav links from parent

--- microk8s/docker/dashboard/src/Nav.js
+++ microk8s/docker/dashboard/src/Nav.js
@@ -1,17 +1,27 @@
-const Nav = () =>
+const Nav = ({ pageKeys, onPageKeySelect }) =>
   <div className="nav has-no-secondary">
     <a className="nav__brand" href="/">
       <img className="nav__logo" src="http://assets.dynatrace.com/global/logos/dynatrace-logo.svg" alt="dynatrace logo" />
     </a>
     <nav id="nav-bar-example1" className="nav__bar">
       <ul className="nav__list nav__list--primary">
+        {
+          pageKeys.map((pageKey, key) =>
+            <li key={key} className="nav__item">
+              <a
+                className="nav__link"
+                onClick={() => onPageKeySelect(pageKey)}
+                href="#"
+              >
+                {pageKey}
+              </a>
+            </li>
+          )
+        }
         {/* <li className="nav__item">
-          <a className="nav__link" href="index.html">ACE-BOX</a>
-        </li> */}
-        <li className="nav__item">
           <a className="nav__link" target="_blank" rel="noreferrer" href={process.env.REACT_APP_JENKINS_URL}>Jenkins</a>
-        </li>
-        <li className="nav__item">
+        </li> */}
+        {/* <li className="nav__item">
           <a className="nav__link" target="_blank" rel="noreferrer" href={process.env.REACT_APP_GITEA_URL}>Gitea</a>
         </li>
         <li className="nav__item">
@@ -22,7 +32,7 @@ const Nav = () =>
         </li>
         <li className="nav__item">
           <a className="nav__link" target="_blank" rel="noreferrer" href={process.env.REACT_APP_DT_TENANT_URL}>Dynatrace Tenant</a>
-        </li>
+        </li> */}
       </ul>
     </nav>
   </div>
