63d9f08ccd2e2358e3cca63e035487b9f3573173
Tobi Gremmer <tobias.gremmer@dynatrace.com>
2021-06-16 08:53:28 +0200

    remove stepper; add tabs

--- microk8s/docker/dashboard/src/how-to/HowTo.js
+++ microk8s/docker/dashboard/src/how-to/HowTo.js
@@ -1,111 +1,96 @@
 import React, { useState } from "react"
-import Cookies from "universal-cookie"
+import { useGitea, useGitlab } from "../libs/credentials"
 
-import Preface from "./Preface"
-import WrapUp from "./WrapUp"
-import StepOne from "./StepOne"
+import Jenkins from "./Jenkins"
+import Gitea from "./Gitea"
+import Gitlab from "./Gitlab"
+import Keptn from "./Keptn"
+import Dynatrace from "./Dynatrace"
+import Kubernetes from "./Kubernetes"
 
-const HowToStepper = ({ activeChildIndex, setActiveChildIndex, children }) => {
-  const allChildren = React.Children.toArray(children)
+const ToolTabs = () => {
+  const { isEnabled: isGitlabEnabled } = useGitlab()
 
-  const setChild = (childIndex) => {
-    setActiveChildIndex(childIndex)
+  const tabs = {
+    "Jenkins": Jenkins,
+    "Gitea": Gitea,
+    "Gitlab": Gitlab,
+    "Keptn": Keptn,
+    "Dynatrace": Dynatrace,
+    "Kubernetes": Kubernetes
   }
 
-  const setPreviousChild = () => {
-    const indexOfNewChild = activeChildIndex - 1
-    const isIndexAllowed = indexOfNewChild >= 0 && indexOfNewChild < allChildren.length
-    
-    if (isIndexAllowed) {
-      setActiveChildIndex(indexOfNewChild)
-    }
+  if (!isGitlabEnabled) {
+    delete tabs["Gitlab"]
   }
 
-  const setNextChild = () => {
-    const indexOfNewChild = activeChildIndex + 1
-    const isIndexAllowed = indexOfNewChild >= 0 && indexOfNewChild < allChildren.length
-    
-    if (isIndexAllowed) {
-      setActiveChildIndex(indexOfNewChild)
-    }
-  }
+  const [activeTab, setActiveTab] = useState(Object.keys(tabs)[0])
+  const ActiveTabContent = tabs[activeTab]
 
   return (
     <>
-      <div className="section">
-        {allChildren[activeChildIndex]}
-      </div>
-      <div style={{ width: "100%", position: "fixed", bottom: "10px" , left: "0", right: "0" }}>
-        <ul className="pagination">
-          <li className="pagination__item">
-            <div
-              className="arrow arrow--reversed"
-              style={{ cursor: "pointer" }}
-              rel="prev"
-              onClick={() => setPreviousChild()}
-            >
-              Previous
-            </div>
-          </li>
-          {
-            allChildren.map((children, key) => {
-              const isActive = key === activeChildIndex
+      <div className="tabs" style={{ marginBottom: "13px" }}>
+        {
+          Object.keys(tabs).map((tab, key) => {
+            const isActive = tab === activeTab
 
-              return (
-                <li key={key} className="pagination__item">
-                  <div
-                    className={isActive ? "pagination__current" : "pagination__link"}
-                    style={{ cursor: "pointer" }}
-                    onClick={() => setChild(key)}
-                  >
-                    {key+1}
-                  </div>
-                </li>
-              )
-            })
-          }
-          <li className="pagination__item">
-            <div
-              className="arrow"
-              style={{ cursor: "pointer" }}
-              rel="next"
-              onClick={() => setNextChild()}
-            >
-              Next
-            </div>
-          </li>
-        </ul>
+            return (
+              <button
+                key={key}
+                className={`tab ${isActive ? "is-active" : ""}`}
+                onClick={() => setActiveTab(tab)}
+              >
+                {tab}
+              </button>
+            )
+          })
+        }
+      </div>
+      <div>
+        <ActiveTabContent />
       </div>
     </>
   )
 }
 
-const HowTo = () => {
-  const cookies = new Cookies()
-  
-  const friendlyActiveStep = cookies.get('how-to-stepper')
-  const initialActiveChildIndex = friendlyActiveStep ? friendlyActiveStep - 1 : 0
-
-  const [activeChildIndex, setActiveChildIndex] = useState(initialActiveChildIndex)
-
-  const setStep = (activeChildIndex) => {
-    const friendlyStep = activeChildIndex + 1
-    cookies.set('how-to-stepper', friendlyStep, { path: '/' })
-
-    setActiveChildIndex(activeChildIndex)
-  }
+const HowTo = ({ isGitlabEnabled }) => {
+  const { href: giteaHref } = useGitea()
 
   return (
     <div>
-      <h2>How-To</h2>
-      <HowToStepper
-        activeChildIndex={activeChildIndex}
-        setActiveChildIndex={setStep}
-      >
-        <Preface />
-        <StepOne />
-        <WrapUp />
-      </HowToStepper>
+      <>
+        <h2>How-To</h2>
+        <p>Welcome to your personal ACE Box! It comes with a set of already configured tools, code, pipelines, etc. to showcase a selection of cloud automation use cases.</p>
+        <p>
+          Before you get started, please familiarize yourself with the Dashboard you're currently looking at:
+        </p>
+        <ul>
+          <li>A <b>How-To</b> gives you step-by-step instructions for each use case.</li>
+          <li>As the name indicates, <b>Deployment Preview</b> shows you the currently deployed version of a demo application up in each stage. Don't worry, the error you're seeing is fine for now as the application is not deployed yet. You will fix this as part of one the first use cases.</li>
+          <li><b>Links</b> lists tools and credentials that you will need while going through the use cases.</li>
+        </ul>
+        <p>
+          In order to ... the following tools have been set up and configured (click on each tab to get an overview of what's been configured):
+        </p>
+      </>
+      <ToolTabs isGitlabEnabled={isGitlabEnabled} />
+      <>
+        <h2>Use Cases</h2>
+        <p>
+          The following list shows use cases currently supported by ACE Box. Please follow the link for step-by-step instructions.
+        </p>
+        <dl className="definition-list">
+          <dt>All-in-one</dt>
+          <dd><a href={`${giteaHref}/ace/ace`} target="_blank" rel="noreferrer">Step-by-step instructions</a></dd>
+          {/* <dt>monaco-gitops-demo</dt>
+          <dd><a href={`${giteaHref}/`}>Step-by-step instructions</a></dd> */}
+          <dt>Monaco - Hands-on Training</dt>
+          <dd><a href={`${giteaHref}/monaco-hot/monaco-hot`}>Step-by-step instructions</a></dd>
+          <dt>Quality Gates - Hands-on Training</dt>
+          <dd><a href={`${giteaHref}/quality-gates-hot/lab-guides`} target="_blank" rel="noreferrer">Step-by-step instructions</a></dd>
+        </dl>
+        {/* <p>In case you have any questions, please reach out to: ...Email / Slack / etc?... You can also find us on ...Github link... </p> */}
+      </>
     </div>
   )
 }
--- microk8s/docker/dashboard/src/how-to/WrapUp.js
+++
@@ -1,8 +0,0 @@
-const WrapUp = () =>
-  <div>
-    <h3>WrapUp</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro et culpa quaerat inventore pariatur impedit tempora recusandae iste nam ipsam eos explicabo eligendi dolorum saepe doloremque, similique tempore rerum ipsum.</p>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur animi qui saepe incidunt reprehenderit laboriosam dolore sit assumenda illo, nisi velit dolores nobis, beatae similique et officia veritatis eligendi. Rem.</p>
-  </div>
-
-export { WrapUp as default }
