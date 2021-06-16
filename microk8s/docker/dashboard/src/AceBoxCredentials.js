c33e5884945de8c5ea5c494a89738d55e0d464eb
Tobi Gremmer <tobias.gremmer@dynatrace.com>
2021-06-16 08:48:35 +0200

    add inline component

--- microk8s/docker/dashboard/src/AceBoxCredentials.js
+++ microk8s/docker/dashboard/src/AceBoxCredentials.js
@@ -6,6 +6,69 @@ const copyToClipboard = (value, setIsCopied) => {
   setIsCopied(true)
 }
 
+const AceBoxCredentialInline = ({ value, type }) => {
+  const isSecret = type === "password"
+
+  const [isShown, setIsShown] = useState(!isSecret)
+  const [isCopied, setIsCopied] = useState(false)
+
+  useEffect(() => {
+    let timeoutId = null
+
+    if (isCopied) {
+      timeoutId = setTimeout(() => {
+        setIsCopied(false)
+      }, 800)
+    }
+    
+    return () => clearTimeout(timeoutId)
+  }, [isCopied])
+
+  return (
+    <>
+      <span style={{ backgroundColor: "#f2f2f2", padding: "4px 6px", borderRadius: "2px" }}>
+        {
+          isShown
+            ? <>{value}</>
+            : <>&bull;&bull;&bull;&bull;&bull;</>
+        }
+      </span>
+      {
+        isSecret &&
+          <span style={{ marginLeft: "5px" }}>
+            <button
+              type="button"
+              className="btn btn--secondary"
+              style={{ border: "none", background: "none", padding: "0px", margin: "0px", height: "12px", lineHeight: "12px", fontSize: "12px" }}
+              onClick={() => setIsShown(isShown => !isShown)}
+            >
+              {
+                isShown
+                  ? "Hide"
+                  : "Show"
+              } 
+            </button>
+          </span>
+      }
+      <span style={{ marginLeft: "5px" }}>
+        <button
+          type="button"
+          className="btn btn--secondary"
+          style={{ border: "none", background: "none", padding: "0px", margin: "0px", height: "12px", lineHeight: "12px", fontSize: "12px" }}
+          onClick={() => copyToClipboard(value, setIsCopied)}
+        >
+          {
+            isCopied
+              ? "Copied!"
+              : "Copy"
+          }
+        </button>
+      </span>
+    </>
+  )
+
+}
+
 const AceBoxCredential = ({ name, value, type }) => {
   const isSecret = type === "password"
 
