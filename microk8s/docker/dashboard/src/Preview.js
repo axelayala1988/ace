217a7ab280415499097ecee3283f7a679d7534cd
Tobi Gremmer <tobias.gremmer@dynatrace.com>
2021-07-13 09:21:11 +0200

    add canary preview

--- microk8s/docker/dashboard/src/Preview.js
+++ microk8s/docker/dashboard/src/Preview.js
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react'
+import { useAwx } from "./libs/credentials"
 
 const Preview = () => {
+  const { isEnabled: isAwxEnabled } = useAwx()
+
   const [iframeKey, setIframeKey] = useState(Math.random().toString(36).substring(7))
 
   useEffect(() => {
@@ -21,16 +24,21 @@ const Preview = () => {
         </div>
         <div className="column column--1-of-2">
           <h3>Production</h3>
-          <iframe title="production" key={iframeKey} id="iframe1" height="600px" width="100%" src={process.env.REACT_APP_SIMPLENODEAPP_URL_PRODUCTION} />
+          <iframe title="production" key={iframeKey} id="iframe2" height="600px" width="100%" src={process.env.REACT_APP_SIMPLENODEAPP_URL_PRODUCTION} />
         </div>
       </div>
-      {/* <h2>Deployment preview - Demo Canary</h2>
-      <div className="section">
-        <div className="column column--1-of-2">
-          <h3>Canary</h3>
-          <iframe title="staging" key={iframeKey} id="iframe1" height="600px" width="100%" src={process.env.REACT_APP_SIMPLENODEAPP_URL_CANARY} />
-        </div>
-      </div> */}
+      {
+        isAwxEnabled &&
+          <>
+            <h2>Deployment preview - Demo Canary</h2>
+            <div className="section">
+              <div className="column column--1-of-2">
+                <h3>Canary</h3>
+                <iframe title="canary" key={iframeKey} id="iframe3" height="600px" width="100%" src={process.env.REACT_APP_SIMPLENODEAPP_URL_CANARY} />
+              </div>
+            </div>
+          </>
+      }
     </div>
   )
 }
