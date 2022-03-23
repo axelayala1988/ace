dfae34c4ec2f56abbaff4b31b6a0d440912e946e
Tobi Gremmer <tobias.gremmer@dynatrace.com>
2022-03-23 15:32:34 +0100

    Update to Ubuntu 20

--- user-skel/init.sh
+++ user-skel/init.sh
@@ -12,23 +12,18 @@ ACE_BOX_USER="${ACE_BOX_USER:-$USER}"
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 
 echo "INIT - Update apt-get and upgrade already install packages..."
-apt-get update && sudo apt-get dist-upgrade -y
+apt-get update && apt-get dist-upgrade -y
 
 echo "INIT - Setting up Python..."
-apt-get install python3.8 -y
-update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
-
-# Update apt-get again to resolve unmet python3.8/pip dependencies
-apt-get update
 apt-get install python3-pip -y
 
 # Upgrade pip
-python3.8 -m pip --version
-python3.8 -m pip install --upgrade pip -q
+python3 -m pip --version
+python3 -m pip install --upgrade pip -q
 
 # Ansible
 echo "INIT - Installing Ansible..."
-sudo -u $ACE_BOX_USER python3.8 -m pip install --user ansible
+python3 -m pip install ansible
 ln -s /home/$ACE_BOX_USER/.local/bin/ansible /usr/bin/ansible
 ln -s /home/$ACE_BOX_USER/.local/bin/ansible-galaxy /usr/bin/ansible-galaxy
 ln -s /home/$ACE_BOX_USER/.local/bin/ansible-playbook /usr/bin/ansible-playbook
@@ -38,7 +33,10 @@ sudo -u $ACE_BOX_USER ansible-galaxy install -r /home/$ACE_BOX_USER/ansible/requ
 
 # Setup ace-cli
 echo "INIT - Setting up ACE-CLI..."
-sudo -u $ACE_BOX_USER python3.8 -m pip install -r /home/$ACE_BOX_USER/.ace/requirements.txt
+
+# Install as root. Packages will be available for all users
+python3 -m pip install -r /home/$ACE_BOX_USER/.ace/requirements.txt
+
 cp /home/$ACE_BOX_USER/.ace/ace /usr/local/bin/ace
 chmod 0755 /usr/local/bin/ace
 
