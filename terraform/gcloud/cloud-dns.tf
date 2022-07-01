a4141ae705599b2f5e4c7500cb3a9d8806c7db9e
Tobi Gremmer <tobias.gremmer@dynatrace.com>
2022-07-01 14:10:47 +0200

    Enable dashboard; Add optional CA services

--- terraform/gcloud/cloud-dns.tf
+++ terraform/gcloud/cloud-dns.tf
@@ -7,3 +7,13 @@ resource "google_dns_record_set" "ace_box" {
   rrdatas      = [google_compute_instance.acebox.network_interface[0].access_config[0].nat_ip]
   ttl          = 300
 }
+
+resource "google_dns_record_set" "ace_box_root" {
+  count = (var.custom_domain == "" || var.managed_zone_name == "") ? 0 : 1
+
+  managed_zone = var.managed_zone_name
+  name         = "${var.custom_domain}."
+  type         = "A"
+  rrdatas      = [google_compute_instance.acebox.network_interface[0].access_config[0].nat_ip]
+  ttl          = 300
+}
