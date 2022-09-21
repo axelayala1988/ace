03dfeb92e849eb5c76d21f9f0a35b5fc433fb556
Tobi Gremmer <tobias.gremmer@dynatrace.com>
2022-09-21 16:31:57 +0200

    Add outputs

--- terraform/aws/t-cloud-summit-infra/outputs.tf
+++ terraform/aws/t-cloud-summit-infra/outputs.tf
@@ -1,3 +1,10 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "subnet_ids" {
+  value = module.vpc.public_subnets
+}
 output "shh" {
   value = "ssh -i ${module.ssh_key.private_key_filename} ${var.acebox_user}@${aws_instance.acebox.public_ip}"
 }
